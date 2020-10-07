0843c343c04a8abe0de4d3ead056c1a2789e80cf
JinUk Lee <>
2020-10-07 21:27:52 +0900

    로고디자인

--- app/src/main/java/com/example/foodkcal_app/Logo.java
+++ app/src/main/java/com/example/foodkcal_app/Logo.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class Logo extends AppCompatActivity {
 
@@ -12,6 +16,10 @@ public class Logo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        ImageView logoImage = (ImageView)findViewById(R.id.logoImage);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+        logoImage.setAnimation(animation);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
